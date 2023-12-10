a3b66179d94e2ce5ce25aa944d78fad1fdc010b3
Silver Horcrux <hltan8495@gmail.com>
2023-12-10 16:39:53 +0700

    Destroy Zone 1

--- starter-code/zone1/project.tf
+++ starter-code/zone1/project.tf
@@ -1,35 +1,35 @@
-resource "kubernetes_namespace" "udacity" {
-   metadata {
-     name = local.name
-   }
-   depends_on = [
-     module.project_eks
-   ]
-}
+# resource "kubernetes_namespace" "udacity" {
+#    metadata {
+#      name = local.name
+#    }
+#    depends_on = [
+#      module.project_eks
+#    ]
+# }
 
-resource "kubernetes_service" "grafana-external" {
-  metadata {
-    name      = "grafana-external"
-    namespace = "monitoring"
-    annotations = {
-      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-    }
-  }
-  spec {
-    selector = {
-      "app.kubernetes.io/name"="grafana"
-    }
+# resource "kubernetes_service" "grafana-external" {
+#   metadata {
+#     name      = "grafana-external"
+#     namespace = "monitoring"
+#     annotations = {
+#       "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+#       "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+#     }
+#   }
+#   spec {
+#     selector = {
+#       "app.kubernetes.io/name"="grafana"
+#     }
 
-    port {
-      port        = 80
-      target_port = 3000
-    }
+#     port {
+#       port        = 80
+#       target_port = 3000
+#     }
 
-    type = "LoadBalancer"
-  }
+#     type = "LoadBalancer"
+#   }
 
-  depends_on = [
-    module.project_eks
-  ]
-}
+#   depends_on = [
+#     module.project_eks
+#   ]
+# }
